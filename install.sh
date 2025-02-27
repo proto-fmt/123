31f6125d999580052d0fb6e0f19910ae31b7c69b
monkey <arch@mail.com>
2025-02-27 18:19:33 +0500

    adsfа

--- install.sh
+++ install.sh
@@ -1,63 +1,12 @@
 #!/bin/bash
 
-# Function to print colored messages
-print_message() {
-    echo -e "\e[1;32m$1\e[0m"
-}
-
-# Function to print error messages
-print_error() {
-    echo -e "\e[1;31m$1\e[0m"
-}
-
-# Function to display a dot animation while a command is running
-dot_animation() {
-    local pid=$1
-    local delay=0.5
-    while [[ $(ps -p $pid -o comm=) != "" ]]; do
-        printf "   \r"
-        sleep $delay
-        printf ".  \r"
-        sleep $delay
-        printf ".. \r"
-        sleep $delay
-        printf "...\r"
-        sleep $delay
-    done
-    printf "    \r"
-}
-
-# Function to execute a command with a dot animation
-execute_with_animation() {
-    local command=$1
-    local description=$2
-    echo -n "$description... "
-    $command &>/dev/null &
-    local pid=$!
-    dot_animation $pid
-    wait $pid
-    if [[ $? -eq 0 ]]; then
-        echo -e "\e[1;32m[OK]\e[0m"
-    else
-        echo -e "\e[1;31m[FAIL]\e[0m"
-        exit 1
-    fi
-}
-
-# Check if the script is run as root
-if [[ $EUID -ne 0 ]]; then
-    print_error "This script must be run as root."
-    exit 1
-fi
-
 ### User Configurable Variables ###
 # Disk to install on (e.g., /dev/sda)
 DISK="/dev/sda"
 
-# Partition sizes (in MiB)
-BOOT_SIZE=512         # Fixed size for /boot (512 MiB)
-SWAP_SIZE=2048        # Size of swap partition (2 GiB)
-ROOT_SIZE=18432       # Size of root partition (18 GiB)
+# Partition sizes (in GB, except for boot which is fixed at 512 MiB)
+SWAP_SIZE=2          # Size of swap partition (in GB)
+ROOT_SIZE=18         # Size of root partition (in GB)
 
 # Localization settings
 TIMEZONE="Europe/Moscow"
@@ -76,6 +25,43 @@ USER_PASSWORD="password"
 
 ### End of User Configurable Variables ###
 
+# Fixed boot partition size (512 MiB)
+BOOT_SIZE_MB=512
+
+# Convert GB to MiB for calculations
+SWAP_SIZE_MB=$((SWAP_SIZE * 1024))
+ROOT_SIZE_MB=$((ROOT_SIZE * 1024))
+
+# Function to print colored messages
+print_message() {
+    echo -e "\e[1;32m$1\e[0m"
+}
+
+# Function to print error messages
+print_error() {
+    echo -e "\e[1;31m$1\e[0m"
+}
+
+# Function to execute a command and display status
+execute_with_status() {
+    local command=$1
+    local description=$2
+    echo -n "$description... "
+    $command &>/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo -e "\e[1;32m[OK]\e[0m"
+    else
+        echo -e "\e[1;31m[FAIL]\e[0m"
+        exit 1
+    fi
+}
+
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+    print_error "This script must be run as root."
+    exit 1
+fi
+
 # Validate user inputs
 validate_inputs() {
     # Check if the disk exists
@@ -87,9 +73,9 @@ validate_inputs() {
     # Check if the disk is large enough
     DISK_SIZE=$(blockdev --getsize64 $DISK)
     DISK_SIZE_MB=$((DISK_SIZE / 1024 / 1024))
-    REQUIRED_SIZE=$((BOOT_SIZE + SWAP_SIZE + ROOT_SIZE + 100)) # 100 MiB buffer
+    REQUIRED_SIZE=$((BOOT_SIZE_MB + SWAP_SIZE_MB + ROOT_SIZE_MB + 100)) # 100 MiB buffer
     if [[ $DISK_SIZE_MB -lt $REQUIRED_SIZE ]]; then
-        print_error "Disk $DISK is too small. Required: $REQUIRED_SIZE MiB, Available: $DISK_SIZE_MB MiB."
+        print_error "Disk $DISK is too small. Required: $((REQUIRED_SIZE / 1024)) GB, Available: $((DISK_SIZE_MB / 1024)) GB."
         exit 1
     fi
 
@@ -114,19 +100,20 @@ validate_inputs() {
 
 # Confirm settings with the user
 confirm_settings() {
-    print_message "Settings:"
-    echo "Disk: $DISK"
-    echo "Boot partition size: $BOOT_SIZE MiB (fixed)"
-    echo "Swap partition size: $SWAP_SIZE MiB"
-    echo "Root partition size: $ROOT_SIZE MiB"
-    echo "Remaining space will be used for /home."
-    echo "Timezone: $TIMEZONE"
-    echo "Locale: $LOCALE"
-    echo "Keymap: $KEYMAP"
-    echo "Hostname: $HOSTNAME"
-    echo "Root password: $ROOT_PASSWORD"
-    echo "Username: $USERNAME"
-    echo "User password: $USER_PASSWORD"
+    print_message "=== Installation Settings ==="
+    echo -e "Disk: \e[1;34m$DISK\e[0m"
+    echo -e "Boot partition size: \e[1;34m512 MiB (fixed)\e[0m"
+    echo -e "Swap partition size: \e[1;34m$SWAP_SIZE GB\e[0m"
+    echo -e "Root partition size: \e[1;34m$ROOT_SIZE GB\e[0m"
+    echo -e "Remaining space will be used for \e[1;34m/home\e[0m."
+    echo -e "Timezone: \e[1;34m$TIMEZONE\e[0m"
+    echo -e "Locale: \e[1;34m$LOCALE\e[0m"
+    echo -e "Keymap: \e[1;34m$KEYMAP\e[0m"
+    echo -e "Hostname: \e[1;34m$HOSTNAME\e[0m"
+    echo -e "Root password: \e[1;34m$ROOT_PASSWORD\e[0m"
+    echo -e "Username: \e[1;34m$USERNAME\e[0m"
+    echo -e "User password: \e[1;34m$USER_PASSWORD\e[0m"
+    echo -e "================================="
 
     read -p "Continue with these settings? (y/n): " CONFIRM
     if [[ $CONFIRM != "y" ]]; then
@@ -137,19 +124,19 @@ confirm_settings() {
 
 # Partition the disk
 partition_disk() {
-    execute_with_animation \
+    execute_with_status \
         "parted -s $DISK mklabel gpt && \
-         parted -s $DISK mkpart primary fat32 1MiB ${BOOT_SIZE}MiB && \
+         parted -s $DISK mkpart primary fat32 1MiB ${BOOT_SIZE_MB}MiB && \
          parted -s $DISK set 1 esp on && \
-         parted -s $DISK mkpart primary linux-swap ${BOOT_SIZE}MiB $((BOOT_SIZE + SWAP_SIZE))MiB && \
-         parted -s $DISK mkpart primary ext4 $((BOOT_SIZE + SWAP_SIZE))MiB $((BOOT_SIZE + SWAP_SIZE + ROOT_SIZE))MiB && \
-         parted -s $DISK mkpart primary ext4 $((BOOT_SIZE + SWAP_SIZE + ROOT_SIZE))MiB 100%" \
+         parted -s $DISK mkpart primary linux-swap ${BOOT_SIZE_MB}MiB $((BOOT_SIZE_MB + SWAP_SIZE_MB))MiB && \
+         parted -s $DISK mkpart primary ext4 $((BOOT_SIZE_MB + SWAP_SIZE_MB))MiB $((BOOT_SIZE_MB + SWAP_SIZE_MB + ROOT_SIZE_MB))MiB && \
+         parted -s $DISK mkpart primary ext4 $((BOOT_SIZE_MB + SWAP_SIZE_MB + ROOT_SIZE_MB))MiB 100%" \
         "Partitioning disk"
 }
 
 # Format partitions
 format_partitions() {
-    execute_with_animation \
+    execute_with_status \
         "mkfs.fat -F32 ${DISK}1 && \
          mkswap ${DISK}2 && \
          mkfs.ext4 ${DISK}3 && \
@@ -159,7 +146,7 @@ format_partitions() {
 
 # Mount partitions
 mount_partitions() {
-    execute_with_animation \
+    execute_with_status \
         "mount ${DISK}3 /mnt && \
          mkdir -p /mnt/boot && \
          mount ${DISK}1 /mnt/boot && \
@@ -171,21 +158,21 @@ mount_partitions() {
 
 # Install base system
 install_base_system() {
-    execute_with_animation \
+    execute_with_status \
         "pacstrap /mnt base linux linux-firmware" \
         "Installing base system"
 }
 
 # Generate fstab
 generate_fstab() {
-    execute_with_animation \
+    execute_with_status \
         "genfstab -U /mnt >> /mnt/etc/fstab" \
         "Generating fstab"
 }
 
 # Configure system
 configure_system() {
-    execute_with_animation \
+    execute_with_status \
         "arch-chroot /mnt ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime && \
          arch-chroot /mnt hwclock --systohc && \
          echo '$LOCALE UTF-8' >> /mnt/etc/locale.gen && \
@@ -201,7 +188,7 @@ configure_system() {
 
 # Install systemd-boot
 install_systemd_boot() {
-    execute_with_animation \
+    execute_with_status \
         "arch-chroot /mnt bootctl --path=/boot install && \
          echo 'default arch' > /mnt/boot/loader/loader.conf && \
          echo 'timeout 3' >> /mnt/boot/loader/loader.conf && \
@@ -215,7 +202,7 @@ install_systemd_boot() {
 
 # Install network utilities
 install_network_utilities() {
-    execute_with_animation \
+    execute_with_status \
         "arch-chroot /mnt pacman -S networkmanager --noconfirm && \
          arch-chroot /mnt systemctl enable NetworkManager" \
         "Installing network utilities"
@@ -223,14 +210,14 @@ install_network_utilities() {
 
 # Set root password
 set_root_password() {
-    execute_with_animation \
+    execute_with_status \
         "arch-chroot /mnt bash -c 'echo root:$ROOT_PASSWORD | chpasswd'" \
         "Setting root password"
 }
 
 # Create a new user
 create_user() {
-    execute_with_animation \
+    execute_with_status \
         "arch-chroot /mnt useradd -m -G wheel -s /bin/bash $USERNAME && \
          arch-chroot /mnt bash -c 'echo $USERNAME:$USER_PASSWORD | chpasswd' && \
          echo '$USERNAME ALL=(ALL) ALL' >> /mnt/etc/sudoers" \
