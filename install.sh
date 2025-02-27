35243d901b08ccc66cbbc836055492da8d1a6542
monkey <arch@mail.com>
2025-02-27 18:04:23 +0500

    adsf

--- install.sh
+++ install.sh
@@ -10,6 +10,40 @@ print_error() {
     echo -e "\e[1;31m$1\e[0m"
 }
 
+# Function to display a dot animation while a command is running
+dot_animation() {
+    local pid=$1
+    local delay=0.5
+    while [[ $(ps -p $pid -o comm=) != "" ]]; do
+        printf "   \r"
+        sleep $delay
+        printf ".  \r"
+        sleep $delay
+        printf ".. \r"
+        sleep $delay
+        printf "...\r"
+        sleep $delay
+    done
+    printf "    \r"
+}
+
+# Function to execute a command with a dot animation
+execute_with_animation() {
+    local command=$1
+    local description=$2
+    echo -n "$description... "
+    $command &>/dev/null &
+    local pid=$!
+    dot_animation $pid
+    wait $pid
+    if [[ $? -eq 0 ]]; then
+        echo -e "\e[1;32m[OK]\e[0m"
+    else
+        echo -e "\e[1;31m[FAIL]\e[0m"
+        exit 1
+    fi
+}
+
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
     print_error "This script must be run as root."
@@ -21,7 +55,7 @@ fi
 DISK="/dev/sda"
 
 # Partition sizes (in MiB)
-BOOT_SIZE=513         # Size of /boot partition (512 MiB)
+BOOT_SIZE=512         # Fixed size for /boot (512 MiB)
 SWAP_SIZE=2048        # Size of swap partition (2 GiB)
 ROOT_SIZE=18432       # Size of root partition (18 GiB)
 
@@ -82,7 +116,7 @@ validate_inputs() {
 confirm_settings() {
     print_message "Settings:"
     echo "Disk: $DISK"
-    echo "Boot partition size: $BOOT_SIZE MiB"
+    echo "Boot partition size: $BOOT_SIZE MiB (fixed)"
     echo "Swap partition size: $SWAP_SIZE MiB"
     echo "Root partition size: $ROOT_SIZE MiB"
     echo "Remaining space will be used for /home."
@@ -103,98 +137,104 @@ confirm_settings() {
 
 # Partition the disk
 partition_disk() {
-    print_message "Partitioning the disk..."
-    parted -s $DISK mklabel gpt
-    parted -s $DISK mkpart primary fat32 1MiB ${BOOT_SIZE}MiB
-    parted -s $DISK set 1 esp on
-    parted -s $DISK mkpart primary linux-swap ${BOOT_SIZE}MiB $((BOOT_SIZE + SWAP_SIZE))MiB
-    parted -s $DISK mkpart primary ext4 $((BOOT_SIZE + SWAP_SIZE))MiB $((BOOT_SIZE + SWAP_SIZE + ROOT_SIZE))MiB
-    parted -s $DISK mkpart primary ext4 $((BOOT_SIZE + SWAP_SIZE + ROOT_SIZE))MiB 100%
+    execute_with_animation \
+        "parted -s $DISK mklabel gpt && \
+         parted -s $DISK mkpart primary fat32 1MiB ${BOOT_SIZE}MiB && \
+         parted -s $DISK set 1 esp on && \
+         parted -s $DISK mkpart primary linux-swap ${BOOT_SIZE}MiB $((BOOT_SIZE + SWAP_SIZE))MiB && \
+         parted -s $DISK mkpart primary ext4 $((BOOT_SIZE + SWAP_SIZE))MiB $((BOOT_SIZE + SWAP_SIZE + ROOT_SIZE))MiB && \
+         parted -s $DISK mkpart primary ext4 $((BOOT_SIZE + SWAP_SIZE + ROOT_SIZE))MiB 100%" \
+        "Partitioning disk"
 }
 
 # Format partitions
 format_partitions() {
-    print_message "Formatting partitions..."
-    mkfs.fat -F32 ${DISK}1
-    mkswap ${DISK}2
-    mkfs.ext4 ${DISK}3
-    mkfs.ext4 ${DISK}4
+    execute_with_animation \
+        "mkfs.fat -F32 ${DISK}1 && \
+         mkswap ${DISK}2 && \
+         mkfs.ext4 ${DISK}3 && \
+         mkfs.ext4 ${DISK}4" \
+        "Formatting partitions"
 }
 
 # Mount partitions
 mount_partitions() {
-    print_message "Mounting partitions..."
-    mount ${DISK}3 /mnt
-    mkdir -p /mnt/boot
-    mount ${DISK}1 /mnt/boot
-    swapon ${DISK}2
-    mkdir -p /mnt/home
-    mount ${DISK}4 /mnt/home
+    execute_with_animation \
+        "mount ${DISK}3 /mnt && \
+         mkdir -p /mnt/boot && \
+         mount ${DISK}1 /mnt/boot && \
+         swapon ${DISK}2 && \
+         mkdir -p /mnt/home && \
+         mount ${DISK}4 /mnt/home" \
+        "Mounting partitions"
 }
 
 # Install base system
 install_base_system() {
-    print_message "Installing base system..."
-    pacstrap /mnt base linux linux-firmware
+    execute_with_animation \
+        "pacstrap /mnt base linux linux-firmware" \
+        "Installing base system"
 }
 
 # Generate fstab
 generate_fstab() {
-    print_message "Generating fstab..."
-    genfstab -U /mnt >> /mnt/etc/fstab
+    execute_with_animation \
+        "genfstab -U /mnt >> /mnt/etc/fstab" \
+        "Generating fstab"
 }
 
 # Configure system
 configure_system() {
-    print_message "Configuring system..."
-    arch-chroot /mnt ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-    arch-chroot /mnt hwclock --systohc
-    echo "$LOCALE UTF-8" >> /mnt/etc/locale.gen
-    arch-chroot /mnt locale-gen
-    echo "LANG=$LOCALE" > /mnt/etc/locale.conf
-    echo "KEYMAP=$KEYMAP" > /mnt/etc/vconsole.conf
-    echo "$HOSTNAME" > /mnt/etc/hostname
-    echo "127.0.0.1 localhost" >> /mnt/etc/hosts
-    echo "::1 localhost" >> /mnt/etc/hosts
-    echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /mnt/etc/hosts
+    execute_with_animation \
+        "arch-chroot /mnt ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime && \
+         arch-chroot /mnt hwclock --systohc && \
+         echo '$LOCALE UTF-8' >> /mnt/etc/locale.gen && \
+         arch-chroot /mnt locale-gen && \
+         echo 'LANG=$LOCALE' > /mnt/etc/locale.conf && \
+         echo 'KEYMAP=$KEYMAP' > /mnt/etc/vconsole.conf && \
+         echo '$HOSTNAME' > /mnt/etc/hostname && \
+         echo '127.0.0.1 localhost' >> /mnt/etc/hosts && \
+         echo '::1 localhost' >> /mnt/etc/hosts && \
+         echo '127.0.1.1 $HOSTNAME.localdomain $HOSTNAME' >> /mnt/etc/hosts" \
+        "Configuring system"
 }
 
 # Install systemd-boot
 install_systemd_boot() {
-    print_message "Installing systemd-boot..."
-    arch-chroot /mnt bootctl --path=/boot install
-    cat <<EOF > /mnt/boot/loader/loader.conf
-default arch
-timeout 3
-console-mode max
-EOF
-    cat <<EOF > /mnt/boot/loader/entries/arch.conf
-title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /initramfs-linux.img
-options root=${DISK}3 rw
-EOF
+    execute_with_animation \
+        "arch-chroot /mnt bootctl --path=/boot install && \
+         echo 'default arch' > /mnt/boot/loader/loader.conf && \
+         echo 'timeout 3' >> /mnt/boot/loader/loader.conf && \
+         echo 'console-mode max' >> /mnt/boot/loader/loader.conf && \
+         echo 'title   Arch Linux' > /mnt/boot/loader/entries/arch.conf && \
+         echo 'linux   /vmlinuz-linux' >> /mnt/boot/loader/entries/arch.conf && \
+         echo 'initrd  /initramfs-linux.img' >> /mnt/boot/loader/entries/arch.conf && \
+         echo 'options root=${DISK}3 rw' >> /mnt/boot/loader/entries/arch.conf" \
+        "Installing systemd-boot"
 }
 
 # Install network utilities
 install_network_utilities() {
-    print_message "Installing network utilities..."
-    arch-chroot /mnt pacman -S networkmanager --noconfirm
-    arch-chroot /mnt systemctl enable NetworkManager
+    execute_with_animation \
+        "arch-chroot /mnt pacman -S networkmanager --noconfirm && \
+         arch-chroot /mnt systemctl enable NetworkManager" \
+        "Installing network utilities"
 }
 
 # Set root password
 set_root_password() {
-    print_message "Setting root password..."
-    arch-chroot /mnt bash -c "echo 'root:$ROOT_PASSWORD' | chpasswd"
+    execute_with_animation \
+        "arch-chroot /mnt bash -c 'echo root:$ROOT_PASSWORD | chpasswd'" \
+        "Setting root password"
 }
 
 # Create a new user
 create_user() {
-    print_message "Creating user $USERNAME..."
-    arch-chroot /mnt useradd -m -G wheel -s /bin/bash $USERNAME
-    arch-chroot /mnt bash -c "echo '$USERNAME:$USER_PASSWORD' | chpasswd"
-    echo "$USERNAME ALL=(ALL) ALL" >> /mnt/etc/sudoers
+    execute_with_animation \
+        "arch-chroot /mnt useradd -m -G wheel -s /bin/bash $USERNAME && \
+         arch-chroot /mnt bash -c 'echo $USERNAME:$USER_PASSWORD | chpasswd' && \
+         echo '$USERNAME ALL=(ALL) ALL' >> /mnt/etc/sudoers" \
+        "Creating user $USERNAME"
 }
 
 # Finalize installation
@@ -202,7 +242,7 @@ finalize_installation() {
     print_message "Installation complete. Unmounting partitions and rebooting..."
     umount -R /mnt
     swapoff -a
-    print_message "You can now reboot into your new Arch Linux system."
+    print_message "System ready for reboot."
 }
 
 ### Main Script ###
